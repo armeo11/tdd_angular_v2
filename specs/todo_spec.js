e1eb2e6329b1b506b00f7ff9cadb6173d1ab111e
pronto.arm <arm@prontomarketing.com>
2013-09-20 15:51:58 +0700

    add save function

--- specs/todo_spec.js
+++ specs/todo_spec.js
@@ -4,11 +4,35 @@ describe('TodoController', function() {
     $scope = $rootScope.$new();
     $controller('TodoController', {
       $scope: $scope,
+      TodoService: null
     });
   }));
 
   it('should have 2 items after initialized', function() {
     expect($scope.items.length).toEqual((2));
   });
-});
+  it('should have Learning Angular as first item', function() {
+    var firstItem = $scope.items[0].name;
+    expect(firstItem).toEqual('Learning Angular');
+  });
+  it('should have Build Angular App as second item', function() {
+    var secondItem = $scope.items[1].name;
+    expect(secondItem).toEqual('Build Angular App');
+  });
 
+  describe('Add Function', function(){
+    it('should have 3 item after added', function(){
+      $scope.newItem = 'Eat lunch';
+      $scope.add();
+      expect($scope.items.length).toEqual(3);
+    });
+    it('should clear new item box after added', function(){
+      $scope.newItem = 'Eat lunch';
+      $scope.add();
+      expect($scope.newItem).toEqual('');
+    });
+  });
+
+  describe('Save Function', function(){
+  });
+});
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,3 +1,42 @@
-function TodoController($scope){
-  $scope.items = ['Learning Angular', 'Build Angular App'];
-}
+var todoApp;
+todoApp = angular.module('TodoApp', ['ngResource']);
+todoApp.factory('TodoService', TodoService);
+injectList = [
+  '$scope',
+  'TodoService',
+  TodoController
+];
+todoApp.controller('TodoController', injectList);
+
+function TodoController($scope, TodoService){
+  $scope.newItem = '';
+  $scope.items = [
+    {"name": "Learning Angular"},
+    {"name": "Build Angular App"}
+  ];
+
+  $scope.add = function(){
+    var item = {name: $scope.newItem};
+    $scope.items.push(item);
+    $scope.newItem = '';
+  }
+
+  $scope.load = function(){
+    $scope.items = TodoService.query();
+  }
+
+  $scope.save = function(){
+    TodoService.save($scope.items)
+  }
+};
+
+function TodoService($resource){
+  var url, defaultParams, actions;
+  url = 'https://armeo11.firebaseio.com/.json';
+  defaultParams = {};
+  actions = {
+    save: { method: 'PUT', isArray: true}
+  };
+
+  return $resource(url, defaultParams, actions);
+};
