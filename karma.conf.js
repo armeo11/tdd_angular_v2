167f3e355d3e2c8a040a5facae221f887c6b4b4a
pronto.arm <arm@prontomarketing.com>
2013-09-20 11:28:36 +0700

    add hello and todo html

--- karma.conf.js
+++ karma.conf.js
@@ -7,10 +7,8 @@ module.exports = function(karma) {
     // base path, that will be used to resolve files and exclude
     basePath: '',
 
-
     // frameworks to use
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -27,38 +25,30 @@ module.exports = function(karma) {
     // templates
     ],
 
-
     // list of files to exclude
     exclude: [
-      
-    ],
 
+    ],
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
     reporters: ['dots'],
 
-
     // web server port
     port: 9876,
-
 
     // cli runner port
     runnerPort: 9100,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
     logLevel: karma.LOG_ERROR,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // Start these browsers, currently available:
     // - Chrome
@@ -70,10 +60,8 @@ module.exports = function(karma) {
     // - IE (only Windows)
     browsers: ['Chrome'],
 
-
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
-
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
